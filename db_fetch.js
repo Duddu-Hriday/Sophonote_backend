fd9de364d8d61c8455db3010d0976ff88b82a76b
Duddu-Hriday <210010016@iitdh.ac.in>
2025-02-11 20:03:52 +0530

    google credentials res.send

--- db_fetch.js
+++ db_fetch.js
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 // Initialize Supabase client
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
